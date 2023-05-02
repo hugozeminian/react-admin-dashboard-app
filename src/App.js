098e87dd9cde7e47b9e523dc8cc07915aa374b12
hugozaminian <hugozamian@hotmail.com>
2023-05-01 22:45:39 -0600

    faq

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
-import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -36,7 +36,7 @@ function App() {
               {/* <Route path="/bar" element={<Bar />} />*/}
               {/* <Route path="/pie" element={<Pie />} />*/}
               {/* <Route path="/line" element={<Line />} />*/}
-              {/* <Route path="/faq" element={<FAQ />} />*/}
+              <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/geography" element={<Geography />} />*/}
             </Routes>
