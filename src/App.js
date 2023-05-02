0c509d143d62e641cb38b0fd3dad1bf3960ffded
hugozaminian <hugozamian@hotmail.com>
2023-05-01 22:41:27 -0600

    calendar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar/calendar";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
@@ -14,7 +15,6 @@ import Form from "./scenes/form";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-// import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -33,12 +33,12 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-             {/* <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
+              {/* <Route path="/bar" element={<Bar />} />*/}
+              {/* <Route path="/pie" element={<Pie />} />*/}
+              {/* <Route path="/line" element={<Line />} />*/}
+              {/* <Route path="/faq" element={<FAQ />} />*/}
               <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />*/}
+              {/* <Route path="/geography" element={<Geography />} />*/}
             </Routes>
           </main>
         </div>
